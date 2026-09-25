2141e39a0b73ac772250f287d58384aa0a270cc5
Govind Sharma <150214417+govindsharma56@users.noreply.github.com>
2026-09-25 16:00:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
-//your JS code here. If required.
+
 let existingbtn = document.querySelector('#existing');
+
 let savedUser = localStorage.getItem('userdata');
-if (savedUser.name) {
-    existingbtn.style.display = 'block';
+
+if (savedUser) {
+    let user = JSON.parse(savedUser);
+
+    if (user.username) {
+        existingbtn.style.display = 'block';
+    }
 }
+
 existingbtn.addEventListener('click', () => {
     let savedUser = localStorage.getItem('userdata');
     let user = JSON.parse(savedUser);
 
-    alert(`Logged in as ${user.name}`);
+    alert(`Logged in as ${user.username}`);
 });
-let submitbtn=document.querySelector('#submit');
-submitbtn.addEventListener('click',(event)=>{ 
-	event.preventDefault()
-let username=document.querySelector('#username').value;
-let password=document.querySelector('#password').value;
-let checkbox=document.querySelector('#checkbox').checked;
-	let user={
-		name:username,
-		password:password
-}
-	if(checkbox){
-		  localStorage.setItem('userdata',JSON.stringify(user));
+
+let submitbtn = document.querySelector('#submit');
+
+submitbtn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    let username = document.querySelector('#username').value;
+    let password = document.querySelector('#password').value;
+    let checkbox = document.querySelector('#checkbox').checked;
+
+    let user = {
+        username: username,
+        password: password
+    };
+
+    if (checkbox) {
+        localStorage.setItem('userdata', JSON.stringify(user));
+    } else {
+        localStorage.removeItem('userdata');
     }
-	else{
-		localStorage.removeItem("userdata");
-	}
-	
-	alert(`Logged in as ${username}`)
-})
+
+    alert(`Logged in as ${username}`);
+});
+
